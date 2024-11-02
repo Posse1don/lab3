f9d9e6da2664e5a217e7a05258b10e686be2fa33
Posse1don <7985858@gmail.com>
2024-11-02 20:37:48 +0200

    extracted new method 'increaseQuality'

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,25 +10,21 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             if (item.name.equals("Aged Brie")
-                    || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality < 50) {
-                            item.quality += 1;
+                || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.quality < 50) {
+                        increaseQuality(item);
 
-                            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                                if (item.sellIn < 11) {
-                                    if (item.quality < 50) {
-                                        item.quality += 1;
-                                    }
-                                }
+                        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            if (item.sellIn < 11) {
+                                    increaseQuality(item);
+                            }
 
-                                if (item.sellIn < 6) {
-                                    if (item.quality < 50) {
-                                        item.quality += 1;
-                                    }
-                                }
+                            if (item.sellIn < 6) {
+                                    increaseQuality(item);
                             }
                         }
-                    } else {
+                    }
+                } else {
                 if (item.quality > 0) {
                     if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                         item.quality -= 1;
@@ -42,21 +38,21 @@ class GildedRose {
 
             if (item.sellIn < 0) {
                 if (item.name.equals("Aged Brie")) {
-                    if (item.quality < 50) {
-                        item.quality += 1;
-                    }
-                } else {
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        increaseQuality(item);
+                } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         item.quality = 0;
-                    } else {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                                item.quality -= 1;
-                            }
+                } else if (item.quality > 0) {
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.quality -= 1;
                         }
                     }
-                }
             }
+        }
+    }
+
+    private static void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality += 1;
         }
     }
 }
