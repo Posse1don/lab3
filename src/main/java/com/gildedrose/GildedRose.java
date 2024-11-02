7479fd2599f7b1d673c0c458fc11a584824de7c8
Posse1don <7985858@gmail.com>
2024-11-02 20:59:58 +0200

    added some code as we want to use switch-case

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -24,22 +24,24 @@ class GildedRose {
                             }
                         }
                     }
-                } else {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        Item.decreaseQuality(item);
-                    }
+                } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    Item.decreaseQuality(item);
                 }
 
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 item.sellIn -= 1;
             }
 
-            if (item.sellIn < 0) {
-                if (item.name.equals("Aged Brie")) {
-                        Item.increaseQuality(item);
-                } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        item.quality = 0;
-                } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (item.name.equals("Aged Brie")) {
+                if (item.sellIn < 0) {
+                    Item.increaseQuality(item);
+                }
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                }
+            } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (item.sellIn < 0) {
                     Item.decreaseQuality(item);
                 }
             }
