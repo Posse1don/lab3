8c4aabbec9a74c54b41156a74182e3ddb754d53b
Posse1don <7985858@gmail.com>
2024-11-03 11:30:21 +0200

    код зі створенням нових об'єктів для класу Main взяв з файлу з тестами для GildedRose. Програма працює коректно, умови рефакторингу виконані

--- src/main/java/com/gildedrose/Main.java
+++ src/main/java/com/gildedrose/Main.java
@@ -1,4 +1,18 @@
 package com.gildedrose;
 
 public class Main {
+    public static void main(String[] args) {
+        Item[] items = new Item[] {
+                new AgedBrie(2, 0),
+                new BackstagePass(15, 20),
+                new Sulfuras(0, 80),
+        };
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        for (Item item : items) {
+            System.out.println(item);
+        }
+    }
 }
